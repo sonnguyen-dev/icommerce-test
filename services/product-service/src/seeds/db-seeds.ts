80457079a796253cf42c9bcd3f0e193e7214e9d3
Son Nguyen <songnob@gmail.com>
2021-01-24 23:50:53 +0700

    Fix db seeds

--- services/product-service/src/seeds/db-seeds.ts
+++ services/product-service/src/seeds/db-seeds.ts
@@ -8,9 +8,9 @@ const brands = ['Brand A', 'Brand B', 'Brand C', 'Brand D'];
 for (let i = 0; i < 1000; i += 1) {
   products.push({
     name: faker.commerce.productName(),
-    price: colors[faker.random.number(colors.length - 1)],
+    price: faker.commerce.price(),
     brand: brands[faker.random.number(brands.length - 1)],
-    color: faker.internet.color(),
+    color: colors[faker.random.number(colors.length - 1)],
   });
 }
 const API_URL = 'http://localhost:9090/api/v1';
